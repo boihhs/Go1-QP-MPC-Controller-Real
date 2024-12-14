1486838a7ee1ed9d18d6be4c7eb2ea0192dccd58
boihhs <84911855+boihhs@users.noreply.github.com>
2024-12-14 18:45:08 -0500

    Update GazeboGo1ROS.h

--- src/Go1_cpp/src/GazeboGo1ROS.h
+++ src/Go1_cpp/src/GazeboGo1ROS.h
@@ -154,7 +154,7 @@ private:
     // variables related to control
     Go1ControlStates go1_ctrl_states;
     Go1RobotControl _root_control;
-    Go1BasicEKF go1_estimate;
+    Go1FilterEKF go1_estimate;
 
     // filters
     MovingWindowFilter acc_x;
